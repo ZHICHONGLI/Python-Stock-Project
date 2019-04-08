5366bd5205dba7509b8c5c24e8286ebccbb2c42b
Luke Li <luke_li2@apple.com>
2019-04-08 16:42:03 -0700

    clean

--- analyze.py
+++ analyze.py
@@ -1,29 +1,30 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing, model_selection, svm
-import quandl
 
-def analyze(data, days = 30):
+
+def analyze(data, days=30):
     df = data
     # set predicting {days} into future
-    forecast_out = int(days) 
+    forecast_out = int(days)
     # data column shifted {days} up
-    df['Prediction'] = df[['Adj Close']].shift(-forecast_out) 
+    df['Prediction'] = df[['Adj Close']].shift(-forecast_out)
     # print(df)
     X = np.array(df.drop(['Prediction'], 1))
     X = preprocessing.scale(X)
     # set X_forecast
-    X_forecast = X[-forecast_out:] 
+    X_forecast = X[-forecast_out:]
     # remove last {days} from X
-    X = X[:-forecast_out] 
+    X = X[:-forecast_out]
     y = np.array(df['Prediction'])
     y = y[:-forecast_out]
 
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size = 0.2)
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(
+        X, y, test_size=0.2)
 
     # Training
     clf = LinearRegression()
-    clf.fit(X_train,y_train)
+    clf.fit(X_train, y_train)
     # Testing
     confidence = clf.score(X_test, y_test)
     print("confidence: ", confidence)
@@ -31,6 +32,7 @@ def analyze(data, days = 30):
     forecast_prediction = clf.predict(X_forecast)
 
     return forecast_prediction
+
 
 def getDateRangeData(stockData):
     dateRangeData = {}
